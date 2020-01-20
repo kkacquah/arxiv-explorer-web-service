2ca23319c952fdc709f1a24011a972c9584289a4
Kenneth Li <kenkraken@gmail.com>
2020-01-20 02:31:14 -0600

    Call ES and move query resolvers to use cases

--- test/data/searchQueryResolverTestQuery.js
+++ test/data/searchQueryResolverTestQuery.js
@@ -2,7 +2,10 @@ module.exports = {
   //List of phrases search for t generate the visualization.
   phrase: "Neural Networks",
   //Interval of the start and end time of the query in milliseconds since epoch.
-  start: 682128000,
-  end: 1577222874,
+  period: {
+    start: 682128000,
+    end: 1577222874,
+    interval: 60*60*24*365
+  },
   category: "cs" //Category as represented by ArXiv short code.
 }
